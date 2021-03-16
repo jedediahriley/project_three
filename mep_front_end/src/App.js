48927bd8150b2abc9599a70d6c9378c7d3ddedc1
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-03-16 01:20:51 -0500

    reorganized file structure

---
+++ mep_front_end/src/App.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "./App.css"
+import Routes from "./Routes"
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentUser: null,
+            userRole: null
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            currentUser: "x",
+            userRole: "Admin"
+        })
+    }
+
+    render() {    
+        return(
+            <div>
+                <h1>Material / Equipment Preservation</h1>
+                <Routes currentUser={this.state.currentUser} userRole={this.state.userRole} />
+            </div>
+        )
+    }
+}
+
+export default App;
