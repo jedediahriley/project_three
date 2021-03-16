7b40b57ccac93b43f368d3e07da66a348f96377b
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-03-16 00:09:22 -0500

    adjusted role booleans to be string value based

--- mep_front_end/src/App/App.js
+++ mep_front_end/src/App/App.js
@@ -5,6 +5,7 @@ import { history } from "../utils/history"
 import Login from "../components/Login/Login"
 import Test1 from "../components/Test"
 import Test2 from "../components/Test2"
+import TechMain from "../components/Tech_User/Main"
 
 // Class component
 class App extends React.Component {
@@ -12,21 +13,19 @@ class App extends React.Component {
         super(props)
         this.state = {
             currentUser: null,
-            isAdmin: false,
-            isQC: false
+            userRole: null
         }
     }
 
     componentDidMount() {
         this.setState({
             currentUser: "x",
-            isAdmin: true,
-            isQC: true
+            userRole: "Tech"
         })
     }
 
     render() {
-        const { currentUser, isAdmin, isQC} = this.state
+        const { currentUser, userRole } = this.state
         // if (!this.state.token) {
         //   return <Login setToken={this.setToken} />
         // }
@@ -38,10 +37,13 @@ class App extends React.Component {
                     {currentUser && 
                         <div>
                             <Route path="/login" component={Login} />
-                            {isAdmin &&
+                            {userRole === "Tech" && 
+                                <Route path="/main" component={TechMain} />
+                            }
+                            {userRole === "Admin" &&
                                 <Route path="/test" component={Test1} />
                             }
-                            {isQC && 
+                            {userRole === "QC" && 
                                 <Route path="/test2" component={Test2} />
                             }
                         </div>
