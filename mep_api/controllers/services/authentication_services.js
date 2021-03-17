7e85367a1011ac1e6938e1ed7e25476276e375df
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-03-16 22:29:20 -0500

    moved services directory

--- mep_api/controllers/services/authentication_services.js
+++
@@ -1,8 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-    if (req.session.currentUser) {
-        next ()
-    }else {
-        console.log("unauthenticated request")
-        res.redirect("/sessions/new")
-    }
-}
